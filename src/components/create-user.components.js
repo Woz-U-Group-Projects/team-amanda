f90272a864f99735272c02b099baf5b841d7c2ae
LillaBee <atrolinger@yahoo.com>
2021-08-02 11:26:13 -0500

    axios step

--- src/components/create-user.components.js
+++ src/components/create-user.components.js
@@ -1,65 +1,69 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
 
-    constructor(props) {
-        super(props)
-    
-        // Setting up functions
-        this.onChangeUserName = this.onChangeUserName.bind(this);
-        this.onChangeUserEmail = this.onChangeUserEmail.bind(this);
-        this.onChangeUserRollno = this.onChangeUserRollno.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    
-        // Setting up state
-        this.state = {
-          name: '',
-          email: '',
-          rollno: ''
-        }
-      }
-    
-      onChangeUserName(e) {
-        this.setState({name: e.target.value})
-      }
-    
-      onChangeUserEmail(e) {
-        this.setState({email: e.target.value})
-      }
-    
-      onChangeUSerRollno(e) {
-        this.setState({rollno: e.target.value})
-      }
-    
-      onSubmit(e) {
-        e.preventDefault()
-    
-        console.log(`User successfully created!`);
-        console.log(`Name: ${this.state.name}`);
-        console.log(`Email: ${this.state.email}`);
-        console.log(`Roll no: ${this.state.rollno}`);
-    
-        this.setState({name: '', email: '', rollno: ''})
-      }
+  constructor(props) {
+    super(props)
+
+    // Setting up functions
+    this.onChangeUserName = this.onChangeUserName.bind(this);
+    this.onChangeUserEmail = this.onChangeUserEmail.bind(this);
+    this.onChangeUserRollno = this.onChangeUserRollno.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    // Setting up state
+    this.state = {
+      name: '',
+      email: '',
+      phone: ''
+    }
+  }
+
+  onChangeUserName(e) {
+    this.setState({ name: e.target.value })
+  }
+
+  onChangeUserEmail(e) {
+    this.setState({ email: e.target.value })
+  }
+
+  onChangeUserPhone(e) {
+    this.setState({ rollno: e.target.value })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const userObject = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone
+    };
+    axios.post('http://localhost:4000/students/create-student', userObject)
+      .then(res => console.log(res.data));
+
+    this.setState({ name: '', email: '', phone: '' })
+  }
 
   render() {
-    return (<div class="form-wrapper">
+    return (<div className="form-wrapper">
       <Form onSubmit={this.onSubmit}>
         <Form.Group controlId="Name">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" value={this.state.name} onChange={this.onChangeUserName}/>
+          <Form.Control type="text" value={this.state.name} onChange={this.onChangeUserName} />
         </Form.Group>
 
         <Form.Group controlId="Email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" value={this.state.email} onChange={this.onChangeUserEmail}/>
+          <Form.Control type="email" value={this.state.email} onChange={this.onChangeUserEmail} />
         </Form.Group>
 
         <Form.Group controlId="Name">
           <Form.Label>Roll No</Form.Label>
-          <Form.Control type="text" value={this.state.rollno} onChange={this.onChangeUserRollno}/>
+          <Form.Control type="text" value={this.state.phone} onChange={this.onChangeUserPhone} />
         </Form.Group>
 
         <Button variant="danger" size="lg" block="block" type="submit">
